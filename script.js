1c0c9ad1f17d46aeea0b082ec6511227cede58a5
ogbstores <132622883+ogbstores@users.noreply.github.com>
2023-05-20 10:40:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let sideMenu = document.querySelector('.side-navigation');
 const menuIcon = document.querySelector(".hamburger i");
 
+let element = document.querySelector('.element');
+let username = document.querySelector('.user-name');
+let drop = document.querySelector('.dropdown');
+
+
 document.querySelector('.hamburger').onclick = () => {
+    element.classList.toggle('active');
+    username.classList.toggle('active');
+    drop.classList.toggle('active');
+
     menuIcon.type = menuIcon.type === "menu" ? "cancel" : "menu";
 
     menuIcon.className = `${menuIcon.type === "menu" ? "bi bi-x" : "bi bi-list"}`
